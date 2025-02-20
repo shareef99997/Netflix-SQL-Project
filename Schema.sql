a20dc9d56e6f8ed1383b85188556c21e98dfcce6
Shareef-taheiya <shareeff@taheiya.sa>
2025-02-20 12:20:14 +0300

    Add Postgresql icon
    notepad "Schema.sql"#

--- Schema.sql
+++ Schema.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- SCHEMAS of Netflix
 
 DROP TABLE IF EXISTS netflix;
@@ -17,4 +18,25 @@ CREATE TABLE netflix
 	description VARCHAR(550)
 );
 
+=======
+-- SCHEMAS of Netflix
+
+DROP TABLE IF EXISTS netflix;
+CREATE TABLE netflix
+(
+	show_id	VARCHAR(5),
+	type    VARCHAR(10),
+	title	VARCHAR(250),
+	director VARCHAR(550),
+	casts	VARCHAR(1050),
+	country	VARCHAR(550),
+	date_added	VARCHAR(55),
+	release_year	INT,
+	rating	VARCHAR(15),
+	duration	VARCHAR(15),
+	listed_in	VARCHAR(250),
+	description VARCHAR(550)
+);
+
+>>>>>>> 5bc094a (Add Postgresql icon)
 SELECT * FROM netflix;
